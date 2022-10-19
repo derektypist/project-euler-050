5197af60952796b38ac7d84f70d808f98a5ef3c2
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-19 10:09:46 +0000

    Added getLimitInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -31,3 +31,21 @@ function isPrime(num) {
         return PRIMESIEVE[parseInt((num-3)/2)];
     }
 }
+
+// Function to Get Limit Information (including Invalid Input)
+function getLimitInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the Input Field
+    let num = document.getElementById("mylimit").value;
+    // Check if the input is valid
+    if (isNaN(num) || num.length == 0 || num<100 || num>1000000 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 100 and 1000000.  Do not include leading zeros.`;
+    } else {
+        txt += `You have entered the limit ${num}. <p>`;
+        txt += `Prime below ${num} is ${consecutivePrimeSum(num)}.`;
+    }
+
+    // Display Information in the Browser
+    document.getElementById("limitinfo").innerHTML = txt;
+}
