f9bc75d52ea1e4d7b6cda76b7ec599a8250de0f8
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-27 10:41:18 +0000

    Added PrimeSieve class at script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
-// Set Up Prime Sieve
-const NUMPRIMES = 1000000;
-const PRIMES = [2];
-const PRIMESIEVE = Array(Math.floor((NUMPRIMES-1)/2)).fill(true);
-(function initPrimes(num) {
-    const upper = Math.floor((num-1)/2);
-    const sqrtUpper = Math.floor((Math.sqrt(num) - 1)/2);
-    for (let i=0;i<=sqrtUpper;i++) {
-        if (PRIMESIEVE[i]) {
-            const prime = 2 * i + 3;
-            PRIMES.push(prime);
-            const primeSquaredIndex = 2 * i ** 2 + 6 * i + 3;
-            for (let j=primeSquaredIndex;j<upper;j+=i) {
-                PRIMESIEVE[j] = false;
+// Set Up PrimeSieve Class
+class PrimeSieve {
+    constructor(num) {
+        const sieve = Array(Math.floor((num-1)/2)).fill(true);
+        const primes = [2];
+        const upper = Math.floor((num-1)/2);
+        const sqrtUpper = Math.floor((Math.sqrt(num)-1)/2);
+
+        for (let i=0;i<=sqrtUpper;i++) {
+            if (sieve[i]) {
+                const prime = 2*i + 3;
+                primes.push(prime);
+                const primeSqInd = 2*i**2 + 6*i + 3;
+                for (let j=primeSqInd;j<upper;j+=prime) {
+                    sieve[j] = false;
+                }
             }
         }
+
+        for (let i=sqrtUpper + 1;i<upper;i++) {
+            if (sieve[i]) primes.push(2*i+3);
+        }
+
+        this._sieve = sieve;
+        this._primes = primes;
     }
 
-    for (let i= sqrtUpper + 1;i<upper;i++) {
-        if (PRIMESIEVE[i]) PRIMES.push(2*i+3);
+    isPrime(num) {
+        return num===2 ? true : num%2===0 ? false : this.isOddPrime(num);
     }
-})(NUMPRIMES);
 
-// Function to check if number num is prime
-function isPrime(num) {
-    if (num===2) {
-        return true;
-    } else if (num % 2===0) {
-        return false;
-    } else {
-        return PRIMESIEVE[parseInt((num-3)/2)];
+    isOddPrime(num) {
+        return this._sieve[parseInt((num-3)/2)];
     }
-}
+
+    get primes() {
+        return this._primes;
+    }
+};
 
 // Function to Show Solution
 function showSolution() {
